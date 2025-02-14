3c10fa8e6ba25f29809e4f75836ca3b225a39ec2
Abraham424 <abrahamdessie1616@gmail.com>
2025-02-14 00:45:36 -0900

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const dbConfig = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'BrokerDatabase',
-});
-module.exports = dbConfig;
